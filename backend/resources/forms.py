491dcb1f4dc7c88dfeb36563a67a397ab784df7f
Gabriel Marton Desidera <gabriel.desidera@daimon.com.br>
2021-06-23 23:38:39 -0300

    Gera POST request funcional com Axios. Finalmente!

--- backend/resources/forms.py
+++ backend/resources/forms.py
@@ -10,12 +10,13 @@ class UploadForm(Resource):
     @classmethod
     def post(cls):
         print('comecou')
-        # print(request)
-        # return {'hello': 'world'}
+        print(request)
+        print(request.data)
+        print(request.files)
         imagem = request.files['imagem']
         imagem.seek(0)
         # file.save(os.path.join(UPLOAD_DIR, file.filename))
-        connection = sqlite3.connect(r'C:\Users\migue\ws-labengsoft\projeto\ProjetoLabsEngSoftRedes\backend\data.db')
+        connection = sqlite3.connect(r'F:\Poli\2021.1\PCS3443\ProjetoLabSoftware\ProjetoLabsEngSoftRedes\backend\data.db')
         connection.execute("""INSERT INTO images ("image_data") values (?)""", [sqlite3.Binary(imagem.read())])
         connection.commit()
         print('sucesso')
