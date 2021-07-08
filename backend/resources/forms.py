a1444ae2dca969dd130c7f00f8cc0d4a10149ff7
Miguel Ferreira <mfc.miguelferreira@gmail.com>
2021-07-08 10:00:25 -0300

    Front end de criação de formulário inicial

--- backend/resources/forms.py
+++ backend/resources/forms.py
@@ -16,7 +16,7 @@ class UploadForm(Resource):
         imagem = request.files['imagem']
         imagem.seek(0)
         # file.save(os.path.join(UPLOAD_DIR, file.filename))
-        connection = sqlite3.connect(r'F:\Poli\2021.1\PCS3443\ProjetoLabSoftware\ProjetoLabsEngSoftRedes\backend\data.db')
+        connection = sqlite3.connect(r'C:\Users\migue\ws-labengsoft\projeto\ProjetoLabsEngSoftRedes\backend\data.db')
         connection.execute("""INSERT INTO images ("image_data") values (?)""", [sqlite3.Binary(imagem.read())])
         connection.commit()
         print('sucesso')
--- frontend/pages/criar_formulario.js
+++ frontend/pages/criar_formulario.js
@@ -1,27 +1,100 @@
-import React from 'react'
-import Link from 'next/link'
-import LayoutCriarFormulario from '../components/layout/LayoutCriarFormulario'
-import { getAppCookies, verifyToken } from '../utilities/util'
-import Image from 'next/image'
+import React, { useState } from 'react'
+//import Link from 'next/link'
+//import LayoutCriarFormulario from '../components/layout/LayoutCriarFormulario'
+//import { getAppCookies, verifyToken } from '../utilities/util'
+//import Image from 'next/image'
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
+import { v4 as uuidv4 } from 'uuid';
 
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  }
+}))
 
-const CriarFormulario = () => {
+function App() {
+  const classes = useStyles()
+  const [inputFields, setInputFields] = useState([
+    { id: uuidv4(), nomeCampo: '' },
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("InputFields", inputFields);
+  };
+
+  const handleChangeInput = (id, event) => {
+    const newInputFields = inputFields.map(i => {
+      if(id === i.id) {
+        i[event.target.name] = event.target.value
+      }
+      return i;
+    })
+    
+    setInputFields(newInputFields);
+  }
+
+  // const handleAddFields = () => {
+  //   setInputFields([...inputFields, { id: uuidv4(), nomeCampo: '' }])
+  // }
+
+  const handleAddFields = () => {
+    setInputFields([...inputFields, { id: uuidv4(), nomeCampo: '' }])
+  }
+
+  const handleRemoveFields = id => {
+    const values  = [...inputFields];
+    values.splice(values.findIndex(value => value.id === id), 1);
+    setInputFields(values);
+  }
 
   return (
-    <LayoutCriarFormulario title="Criar Formulário">
-      <div className="container">
-        <main>
-          <Image
-            src='/Screenshot_3.png'
-            width='821'
-            height='554'
-          />
-          <br></br><br></br>
-        </main>
-      </div>
-    </LayoutCriarFormulario>
-  )
+    <Container>
+      <h1>Criar Formulário</h1>
+      <form className={classes.root} onSubmit={handleSubmit}>
+        { inputFields.map(inputField => (
+          <div key={inputField.id}>
+            <TextField
+              name="nomeCampo"
+              label="Nome do Campo"
+              variant="filled"
+              value={inputField.nomeCampo}
+              onChange={event => handleChangeInput(inputField.id, event)}
+            />
+            <IconButton disabled={inputFields.length === 1} onClick={() => handleRemoveFields(inputField.id)}>
+              <RemoveIcon />
+            </IconButton>
+            <IconButton
+              onClick={handleAddFields}
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
+        )) }
+        <Button
+          className={classes.button}
+          variant="contained" 
+          color="Primary"
+          type="submit" 
+          startIcon={<PictureAsPdfIcon></PictureAsPdfIcon>}
+          onClick={handleSubmit}
+        >Gerar PDF</Button>
+      </form>
+    </Container>
+  );
 }
 
-export default CriarFormulario
+export default App;
