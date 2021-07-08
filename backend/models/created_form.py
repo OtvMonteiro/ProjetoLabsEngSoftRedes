f157fccaa21e9c84c7ed145bfbdb88bfd8a20d7f
Miguel Ferreira <mfc.miguelferreira@gmail.com>
2021-07-08 10:35:48 -0300

    created form modificado

--- backend/models/created_form.py
+++ backend/models/created_form.py
@@ -3,7 +3,7 @@ from db import db
 
 class CreatedFormModel(db.Model):
     __tablename__ = "createdform"
-    id_formulário = db.Column(db.Integer, primary_key=True)
+    id_formulario = db.Column(db.Integer, primary_key=True)
     qr_code = db.Column(db.LargeBinary, unique=True)
     pdf = db.Column(db.LargeBinary)
     municipio = db.Column(db.String(80), nullable=False)
@@ -12,7 +12,7 @@ class CreatedFormModel(db.Model):
 class FieldsForm(db.Model):
     __tablename__ = "fieldsform"
     id_campo = db.Column(db.Integer, primary_key=True)
-    id_formulário = db.Column(db.Integer, db.ForeignKey("createdform.id"), nullable=False)
+    id_formulario = db.Column(db.Integer, db.ForeignKey("createdform.id_formulario"), nullable=False)
     nome = db.Column(db.String(80), nullable=False)
     posicao = db.Column(db.Integer, nullable=False)
     tamanho = db.Column(db.Integer, nullable=False)
